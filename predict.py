0552f9985fae9d07dfbd9c4fb1d27e1c6f5efcef
Lynn Bradshaw <readyready15728@gmail.com>
2018-10-13 08:55:02 -0400

    Removing unnecessary imports

--- predict.py
+++ predict.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import pickle
 from pathlib import Path
-from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV, train_test_split
 
